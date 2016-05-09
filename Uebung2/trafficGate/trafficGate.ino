1af93c5e3f975b03e097b31cd2eae66fefc46172
Michael Schreier <Michael Schreier>
2016-05-09 11:38:45 +0200

    Servo Test and Final Traffic Gate

---
+++ Uebung2/trafficGate/trafficGate.ino
@@ -0,0 +1,102 @@
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h> 
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN  6 // Echo Pin
+#define trigPin 7 // Trigger Pin
+#define echoPin 8 // Echo Pin
+#define PIN_ECHO 8
+
+#define NUM_PIXELS 12
+
+int maximumRange = 20; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+Servo myservo;
+int pos = 0;
+int SERVOpin = 9;
+int servoFix = 25;
+
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+  Serial.begin (9600);
+  ring.begin();
+  ring.show(); // Initialize all pixels to 'off'
+  myservo.attach(SERVOpin); // attaches the servo on pin 9 to the servo object 
+  //Serial.begin(9600);
+}
+int inc = 0;
+int color = 0;
+void loop() {
+   digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2); 
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+ 
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = duration/58.2;
+  Serial.println(distance);
+  if ((distance >= maximumRange) && pos != 0){
+    myservo.write(90+servoFix);
+    setRed();
+   pos = 0;
+  }
+  else if(!(distance >= maximumRange) && pos != 1) {
+   /* Send the distance to the computer using Serial protocol, and
+   turn LED OFF to indicate successful reading. */
+    Serial.println(distance);
+    myservo.write(0+servoFix);
+    setGreen();
+    pos = 1;
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
+}
+
+void setGreen(){
+  for (uint16_t px=0; px<NUM_PIXELS; px++){
+    //setPixel(px,firstSensor/4,0,0);  
+    setPixel(px,0,255,0);
+  }
+}
+
+/*void setYellow(){
+  for (uint16_t px=0; px<NUM_PIXELS; px++){
+    //setPixel(px,firstSensor/4,0,0);  
+    setPixel(px,0,255,255);
+  }
+}*/
+
+void setRed(){
+  for (uint16_t px=0; px<NUM_PIXELS; px++){
+    //setPixel(px,firstSensor/4,0,0);  
+    setPixel(px,255,0,0);
+  }
+}
+
+void setPixel(uint16_t pixel, char r, char g, char b){
+  uint32_t color = ring.Color(r, g, b);
+  ring.setPixelColor(pixel, color);
+  ring.show();
+}
+
+void setPixelC(uint16_t pixel, uint32_t color){
+  ring.setPixelColor(pixel, color);
+  ring.show();
+}
+
